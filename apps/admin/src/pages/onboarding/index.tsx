20b25c929b9742dddb64f8102ed146f36dc703c9
Jordan A holt <jordanh316@gmail.com>
2026-08-08 02:02:51 +0000

    changes

---
+++ apps/admin/src/pages/onboarding/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { OnboardingWizard } from '@onboarding';
+
+const AdminOnboardingPage: React.FC = () => {
+  return <OnboardingWizard />;
+};
+
+export default AdminOnboardingPage;
---
+++ apps/tenant-portal/src/pages/onboarding/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { OnboardingWizard } from '@onboarding';
+
+const TenantOnboardingPage: React.FC = () => {
+  return <OnboardingWizard />;
+};
+
+export default TenantOnboardingPage;
